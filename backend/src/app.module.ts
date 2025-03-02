35b76eb75f44e0492c41f1375f803075f6f60913
quangdang46 <tranquangdang21@gmail.com>
2025-03-02 21:28:33 +0700

    be

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Đọc biến môi trường từ .env
-    DatabaseModule, // Import DatabaseModule
+    DatabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
